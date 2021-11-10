598d81be5717faec638ecfc0413e8db84717ea78
snehasish4u <36485794+snehasish4u@users.noreply.github.com>
2021-11-10 17:07:14 +0530

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmployeeManagement.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Logout()
+        {
+            return View();
+        }
+    }
+}
